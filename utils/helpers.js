3d0d12b330ca803878f3160a60209f07afb4f26b
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-05-05 16:09:28 -0400

    Added helper function formatDate

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,2 +1,23 @@
 // write function to shorten conte to "description"
 // write function to format dates
+
+module.exports = {
+    // render date
+    formatDate: (timestamp) => {
+    var date = new Date(timestamp);
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+
+    if (minutes < 10) {
+    minutes = '0' + minutes;
+    }
+
+    //var time = `${month}/${day}/${year} ${hour}:${minutes}`;
+    var time = `${month}/${day}/${year} at ${hour}:${minutes}`;
+
+    return time;
+    }
+}
