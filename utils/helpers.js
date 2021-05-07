c0c2dc9cdaa1bf0f2c7b47687c0e40347b20fcf7
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-05-07 00:15:19 -0400

    Fixed timezone problem

--- utils/helpers.js
+++ utils/helpers.js
@@ -3,6 +3,28 @@
 module.exports = {
     // render date
     formatDate: (timestamp) => {
-        return timestamp.toLocaleString();
+    var date = new Date(timestamp);
+    var month = date.getMonth() + 1;
+    var day = date.getDate();
+    var year = date.getFullYear();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var amPM = 'AM';
+
+    // add a zero if minutes less than 10
+    if (minutes < 10) {
+    minutes = '0' + minutes;
+    }
+
+    // adjust to 12 hour clock (even though military is better!)
+    if (hour > 12) {
+        amPM = 'PM';
+        hour -= 12;
+    }
+
+    // format time string
+    var time = `${month}/${day}/${year} at ${hour}:${minutes} ${amPM}`;
+
+    return time;
     }
 }
